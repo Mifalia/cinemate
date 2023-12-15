80f1fcb2781574fdab4df2214cd3ef4a8164f52f
mifalia <mifarivonjisoa@gmail.com>
2023-12-15 15:34:11 +0300

    create a structural working router skeleton with react-router-dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,19 @@
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+// component imports
+import HomePage from './pages/home/HomePage';
+
+// assets import
 import './App.css';
 
 function App() {
   return (
-    <div className='App'>
-      <p className='text-primary'>Hello world</p>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path={`/`} element={<HomePage />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
