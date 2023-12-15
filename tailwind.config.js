8e70331e20a2ebb7b1706effa4264177ae81d876
mifalia <mifarivonjisoa@gmail.com>
2023-12-15 15:14:20 +0300

    set up a new react project: _ installed necessary dependancies _ added a jsconfig file _ arranged project directories structure _ configure and set up tailwindcss _ add a jsconfig file for the compiler _ remove unecessary files installed by default with cra

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/**/*.{html,js,jsx}'],
+  theme: {
+    extend: {
+      colors: {
+        'primary-text': 'var(--primary-text)',
+        'secondary-text': 'var(--secondary-text)',
+        'primary-background': 'var(--primary-background)',
+        'elevation-1': 'var(--elevation-1)',
+        'elevaton-2': 'var(--elevation-2)',
+        primary: 'var(--primary)',
+      },
+    },
+  },
+  plugins: [],
+};
