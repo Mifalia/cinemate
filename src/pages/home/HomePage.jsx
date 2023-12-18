a93be03620d7e15ea058a65701983cb1d2e15fe7
mifalia <mifarivonjisoa@gmail.com>
2023-12-18 16:22:54 +0300

    custom header behavior

--- src/pages/home/HomePage.jsx
+++ src/pages/home/HomePage.jsx
@@ -1,5 +1,8 @@
 import React, { useEffect } from 'react';
 import { Link } from 'react-router-dom';
+import { RiSearchLine } from 'react-icons/ri';
+import { IoMdArrowForward } from 'react-icons/io';
+import { AiFillGithub } from 'react-icons/ai';
 
 // assets import
 import s from './HomePage.module.css';
@@ -8,6 +11,18 @@ import app_logo from 'assets/images/cinemate-logo.png';
 function HomePage() {
   useEffect(() => {
     document.title = 'Welcome To Cinemate';
+
+    let header = document.getElementById('app_header');
+
+    window.addEventListener('scroll', () => {
+      if (window.scrollY.valueOf() > 16) {
+        header.classList.remove('fixed', 'bg-transparent');
+        header.classList.add('sticky', 'bg-elevation-2');
+      } else {
+        header.classList.remove('sticky', 'bg-elevation-2');
+        header.classList.add('fixed', 'bg-transparent');
+      }
+    });
   }, []);
 
   return (
@@ -15,6 +30,7 @@ function HomePage() {
       className={`${s['main-wrapper']} flex flex-col min-h-screen text-primary-text`}
     >
       <header
+        id='app_header'
         className={`${s['header-wrapper']} h-20 fixed top-0 right-0 left-0 z-10`}
       >
         <div className='container h-full px-5 mx-auto flex items-center'>
@@ -25,7 +41,10 @@ function HomePage() {
             <Link to='/'>Home</Link>
             <Link to='/'>Search</Link>
             <Link to='/'>Discover</Link>
-            <Link to='/'>About</Link>
+            <Link to='/'>About</Link>|
+            <a href='https://github.com/Mifalia/cinemate'>
+              <AiFillGithub size={24} />
+            </a>
           </nav>
         </div>
       </header>
@@ -40,15 +59,28 @@ function HomePage() {
             explore the world of TV Shows & more
           </p>
           <p className='mb-12'>
-            <Link className='underline text-base'>Learn more about us</Link>
+            <Link className={`underline text-base`}>
+              Learn more about us
+              <IoMdArrowForward size={16} className='ml-2 inline-block' />
+            </Link>
           </p>
-          <button className='rounded-xl border-2 border-primary px-8 py-4 text-primary-text bg-transparent font-bold hover:text-primary-background hover:bg-primary'>
+          <button className='rounded-xl border-2 border-primary flex items-center gap-x-3 pl-6 pr-8 py-4 text-primary-text bg-transparent font-medium hover:text-primary-background hover:bg-primary'>
+            <RiSearchLine size={18} />
             Start exploring
           </button>
         </div>
       </section>
       <footer className='h-min py-6 mt-auto text-center bg-elevation-2'>
-        <div className='container mx-auto'>Footer content</div>
+        <div className='container mx-auto'>
+          © Copyright 2023 -{' '}
+          <a
+            className='text-primary text-sm hover:underline'
+            href='https://github.com/Mifalia'
+          >
+            Mifalia Nathanaël
+          </a>{' '}
+          | All rights reserved
+        </div>
       </footer>
     </div>
   );
