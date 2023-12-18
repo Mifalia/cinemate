5e72e803c671dc75194e51db3261db3f6841d124
mifalia <mifarivonjisoa@gmail.com>
2023-12-18 13:34:42 +0300

    download font files and logo into project directory

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
+// assets import
+import 'assets/css/_global.css';
+
 // component imports
 import HomePage from 'pages/home/HomePage';
 
--- src/pages/home/HomePage.jsx
+++ src/pages/home/HomePage.jsx
@@ -1,4 +1,5 @@
 import React, { useEffect } from 'react';
+import app_logo from 'assets/images/cinemate-logo.png';
 
 import { Link } from 'react-router-dom';
 
@@ -11,7 +12,9 @@ function HomePage() {
     <div className='app-container flex flex-col min-h-screen bg-primary-background text-primary-text'>
       <header className='header-container h-20 bg-elevation-2 fixed top-0 right-0 left-0'>
         <div className='container h-full px-5 mx-auto flex items-center'>
-          <div className='header-brand'>CineMate</div>
+          <div className='header-brand'>
+            <img src={app_logo} alt='Cinemate Logo' />
+          </div>
           <nav className='header-nav ml-auto flex justify-end gap-x-6'>
             <Link to='/'>Home</Link>
             <Link to='/'>Search</Link>
@@ -22,8 +25,10 @@ function HomePage() {
       </header>
       <section className='hero container mx-auto px-5 min-h-screen py-24 flex flex-col justify-center'>
         <div>
-          <h1>Welcome to cinemate</h1>
-          <p>Discover the world of movies & TV Shows</p>
+          <h1 className='section-title'>Welcome to cinemate</h1>
+          <p className='section-description'>
+            Discover the world of movies & TV Shows
+          </p>
         </div>
       </section>
       <footer className='h-min py-6 mt-auto text-center bg-elevation-2'>
