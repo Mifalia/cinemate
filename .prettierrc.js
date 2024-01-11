9ad10d9abe91182b27e9152df6d024ab6b9138de
mifalia <mifarivonjisoa@gmail.com>
2024-01-11 15:42:10 +0300

    add .prettierrc file

---
+++ .prettierrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+  printWidth: null, // Maximum line length
+  useTabs: false, // Use spaces instead of tabs
+  tabWidth: 2, // Visual width of a tab
+  trailingComma: 'es5', // Add trailing commas in objects, arrays, etc.
+  semi: true, // Add ';' when needed
+  singleQuote: true, // Use '' for strings instead of ""
+  bracketSpacing: true, // Include spaces between brackets in object literals
+  arrowParens: 'always', // Always include parentheses around a single arrow function parameter
+  jsxSingleQuote: true, // Use "" for React props, like in HTML
+  bracketSameLine: true, // Place the closing bracket of a multi-line JSX element at the end of the last line instead of a new line
+  endOfLine: 'lf', // Use 'lf' for Linux, 'crlf' for Windows
+};
--- src/pages/home/HomePage.jsx
+++ src/pages/home/HomePage.jsx
@@ -13,40 +13,48 @@ function HomePage() {
   useEffect(() => {
     document.title = 'Welcome To Cinemate';
     // homepage scroll behavior handling
-    var lastScrollTop = 0;
-    var viewportHeight = window.innerHeight;
-    window.addEventListener('scroll', function () {
-      var currentScrollTop = window.scrollY || document.documentElement.scrollTop;
+    // var lastScrollTop = 0;
+    // var viewportHeight = window.innerHeight;
+    // window.addEventListener('scroll', function () {
+    //   var currentScrollTop =
+    //     window.scrollY || document.documentElement.scrollTop;
 
-      if (currentScrollTop > lastScrollTop) {
-        // L'utilisateur fait défiler vers le bas
-        if (window.scrollY.valueOf() > 72 && window.scrollY.valueOf() < viewportHeight) {
-          window.scrollTo({
-            top: viewportHeight,
-            behavior: 'smooth',
-          });
-        }
-      } else if (currentScrollTop < lastScrollTop) {
-        if (window.scrollY.valueOf() < 80) {
-          window.scrollTo({
-            top: 0,
-            behavior: 'smooth',
-          });
-        }
-      }
-      lastScrollTop = currentScrollTop; // Mettez à jour la position précédente du défilement
-    });
+    //   if (currentScrollTop > lastScrollTop) {
+    //     // L'utilisateur fait défiler vers le bas
+    //     if (
+    //       window.scrollY.valueOf() > 72 &&
+    //       window.scrollY.valueOf() < viewportHeight
+    //     ) {
+    //       window.scrollTo({
+    //         top: viewportHeight,
+    //         behavior: 'smooth',
+    //       });
+    //     }
+    //   } else if (currentScrollTop < lastScrollTop) {
+    //     if (window.scrollY.valueOf() < 80) {
+    //       window.scrollTo({
+    //         top: 0,
+    //         behavior: 'smooth',
+    //       });
+    //     }
+    //   }
+    //   lastScrollTop = currentScrollTop; // Mettez à jour la position précédente du défilement
+    // });
   }, []);
 
   return (
-    <div className={`${s['main-wrapper']} flex flex-col min-h-screen text-primary-text`}>
+    <div
+      className={`${s['main-wrapper']} flex flex-col h-[calc(100vh-48px)] text-primary-text`}>
       <Header />
-      <section className={`${s['hero']} container mx-auto px-5 min-h-screen py-24 flex flex-col justify-center`}>
+      <section
+        className={`${s['hero']} container mx-auto px-5 min-h-full py-24 flex flex-col justify-center`}>
         <div className='w-1/2'>
           <h1 className='section-title text-3xl font-bold mb-2'>
             Welcome to <span className='text-primary'>Cinemate</span>
           </h1>
-          <p className='section-description text-6xl font-bold mb-6 capitalize'>explore the world of TV Shows & more</p>
+          <p className='section-description text-6xl font-bold mb-6 capitalize'>
+            explore the world of TV Shows & more
+          </p>
           <p className={`${s['body-link-wrapper']} mb-12 w-fit`}>
             <Link className={`underline text-base leading-loose`}>
               Learn more about us
@@ -62,10 +70,16 @@ function HomePage() {
 
       <section className='w-full bg-primary-background py-14'>
         <div className='container mx-auto'>
-          <h2 className='capitalize  text-center text-3xl font-bold font-sans mb-9'>You may like ...</h2>
+          <h2 className='capitalize  text-center text-3xl font-bold font-sans mb-9'>
+            You may like ...
+          </h2>
           <div className='grid grid-cols-5 gap-6'>
             {/* component */}
-            <MediaMiniature thumbnail='eU1i6eHXlzMOlEq0ku1Rzq7Y4wA.jpg' title='The mandalorian with very long title' year='2021' />
+            <MediaMiniature
+              thumbnail='eU1i6eHXlzMOlEq0ku1Rzq7Y4wA.jpg'
+              title='The mandalorian with very long title'
+              year='2021'
+            />
             {/* component */}
           </div>
         </div>
