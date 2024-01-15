a717d64e2cfa0a80fc05dfd4d4ec95acf248ffac
mifalia <mifarivonjisoa@gmail.com>
2024-01-15 13:29:25 +0300

    add MediaApi service for api request handling

--- src/services/MediaApi.js
+++ src/services/MediaApi.js
@@ -1,6 +1,6 @@
 class MediaApi {
   // proterties
-  _base_url = 'https://api.themoviedb.org/3/trending/';
+  _base_url = 'https://api.themoviedb.org/3/';
   _api_key;
   _options = {};
 
@@ -23,17 +23,89 @@ class MediaApi {
 
   // trending TV shows of the moment
   async getTrending() {
-    let path = `${this._base_url}tv/week?language=en-US`;
+    let endpoint = `${this._base_url}trending/tv/week?language=en-US`;
     try {
-      let response = await fetch(path, this._options);
+      let response = await fetch(endpoint, this._options);
       if (!response.ok) {
-        console.log({ response });
-        throw Error('Unable to connect to the service');
+        throw new Error('Unable to connect to the service');
       }
       response = await response.json();
       return response;
     } catch (error) {
-      console.error(error);
+      this.handleClientError(error);
+      return null;
     }
   }
+
+  // get all time popular TV Shows
+  async getPopular(page = 1) {
+    let endpoint = `${this._base_url}tv/popular?language=en-US&page=${page}`;
+    try {
+      let response = await fetch(endpoint, this._options);
+      if (!response.ok) {
+        throw new Error('Unable to connect to the service at "getPopular"');
+      }
+      response = await response.json();
+      return response;
+    } catch (error) {
+      this.handleClientError(error);
+      return null;
+    }
+  }
+
+  // search TV Shows from keyword `${q}`
+  async search(q = '', page = 1) {
+    q = q.trim();
+    if (!q) {
+      throw new Error('You must provide a keyword to search');
+    }
+
+    let endpoint = `${this._base_url}search/tv?query=${q}&include_adult=false&language=en-US&page=${page}`;
+
+    try {
+      let response = await fetch(endpoint, this._options);
+      if (!response.ok) {
+        throw new Error('Unable to connect to the service at "search"');
+      }
+
+      response = await response.json();
+      return response;
+    } catch (error) {
+      this.handleClientError(error);
+      return null;
+    }
+  }
+
+  // get by id
+  async getById(id) {
+    if (!id) {
+      throw new Error('No tv series ID provided');
+    }
+
+    let endpoint = `${this._base_url}tv/${id}?language=en-US`;
+
+    try {
+      let response = await fetch(endpoint, this._options);
+      if (!response.ok) {
+        throw new Error('Unable to connect to the service at "getById"');
+      }
+
+      response = await response.json();
+      return response;
+    } catch (error) {
+      this.handleClientError(error);
+      return null;
+    }
+  }
+
+  /*
+    SELF RELATED METHODS
+  */
+
+  // error handling
+  handleClientError(error) {
+    console.error(`API request failed : ${error.message}`);
+  }
 }
+
+export default MediaApi;
