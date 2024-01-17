9df837466cd0cc303fa7c2604228cb3ed40703be
mifalia <mifarivonjisoa@gmail.com>
2024-01-17 15:08:29 +0300

    add discover page, route, and link

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 // component imports
 import HomePage from 'pages/home/HomePage';
 import SearchPage from 'pages/search/SearchPage';
+import DiscoverPage from 'pages/discover/DiscoverPage';
 
 // assets import
 import './App.css';
@@ -14,6 +15,7 @@ function App() {
       <Routes>
         <Route path={`/`} element={<HomePage />} />
         <Route path={`/search`} element={<SearchPage />} />
+        <Route path={`/discover`} element={<DiscoverPage />} />
       </Routes>
     </BrowserRouter>
   );
