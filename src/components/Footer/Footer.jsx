941f677c2ef6bda1b263c806551d52b6966ee267
mifalia <mifarivonjisoa@gmail.com>
2024-01-22 13:47:37 +0300

    fix footer responsiveness

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -3,14 +3,9 @@ import React from 'react';
 function Footer() {
   return (
     <footer className='h-min py-6 mt-auto text-center bg-elevation-2'>
-      <div className='container mx-auto'>
+      <div className='container mx-auto text-xs sm:text-sm text-secondary-text'>
         © Copyright 2023 -{' '}
-        <a
-          className='text-primary text-sm hover:underline'
-          href='https://github.com/Mifalia'
-          target='_blank'
-          rel='noreferrer'
-        >
+        <a className='text-primary hover:underline' href='https://github.com/Mifalia' target='_blank' rel='noreferrer'>
           Mifalia Nathanaël
         </a>{' '}
         | All rights reserved
