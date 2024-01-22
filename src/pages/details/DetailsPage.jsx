741247870f5b750ba2040bd6a94cc4f58c750d69
mifalia <mifarivonjisoa@gmail.com>
2024-01-22 11:43:53 +0300

    add list of seasons on details page

--- src/pages/details/DetailsPage.jsx
+++ src/pages/details/DetailsPage.jsx
@@ -17,63 +17,99 @@ function DetailsPage() {
     MediaStore.getOne(id);
   }, [id]);
 
+  useEffect(() => {
+    document.title = MediaStore.current.name ? MediaStore.current.name : 'Details';
+  }, [MediaStore.current]);
+
   return (
     <HeaderFooterLayout>
       {!MediaStore.isLoading ? (
         MediaStore.current && (
-          <div className='pt-24 min-h-[calc(100vh-72px)] text-primary-text'>
-            <section className='container mx-auto h-full px-4'>
-              {/* padded content */}
-              <div className='w-2/3 mt-14 flex flex-col justify-center items-start'>
-                {/* Media TITLE */}
-                <h2 className='uppercase text-4xl font-bold'>{MediaStore.current.name}</h2>
-                {/* Media genres */}
-                <p className='text-secondary-text text-sm mt-1'>
-                  {MediaStore.current.genres?.map((genre, index) => (
-                    <span key={index} className='mr-2 italic'>
-                      #{genre.name}
+          <>
+            <div className='pt-24 min-h-[calc(100vh-72px)] pb-10 text-primary-text'>
+              <section className='container mx-auto h-full px-4'>
+                {/* padded content */}
+                <div className='w-2/3 mt-14'>
+                  {/* Media TITLE */}
+                  <h2 className='uppercase text-4xl font-bold'>{MediaStore.current.name}</h2>
+                  {/* Media genres */}
+                  <p className='text-secondary-text text-sm mt-1'>
+                    {MediaStore.current.genres?.map((genre, index) => (
+                      <span key={index} className='mr-2 italic'>
+                        #{genre.name}
+                      </span>
+                    ))}
+                  </p>
+                  {/* Media rating and votes */}
+                  <div className='flex gap-2 items-end text-primary text-base mb-6'>
+                    <StarRating rating={MediaStore.current.vote_average && MediaStore.current.vote_average / 2} />
+                    <span className='text-sm'>
+                      ({MediaStore.current.vote_count && MediaStore.current.vote_count} votes)
                     </span>
-                  ))}
-                </p>
-                {/* Media rating and votes */}
-                <div className='flex gap-2 items-end text-primary text-base mb-6'>
-                  <StarRating rating={MediaStore.current.vote_average && MediaStore.current.vote_average / 2} />
-                  <span className='text-sm'>
-                    ({MediaStore.current.vote_count && MediaStore.current.vote_count} votes)
-                  </span>
-                </div>
-                {/* Media official tagline if exist */}
-                {MediaStore.current.tagline && (
-                  <p className='italic text-secondary-text text-base mb-4'> {MediaStore.current.tagline} </p>
-                )}
-                {/* Media overview */}
-                <h3 className='text-2xl font-bold'>Overview</h3>
-                <p className='mt-2'>{MediaStore.current.overview && MediaStore.current.overview}</p>
-                {/* button group */}
-                <div className='mt-7 w-fit flex gap-5'>
-                  <button
-                    type='button'
-                    className='text-sm rounded-xl border-2 border-primary flex items-center gap-x-2 pl-3 pr-4 py-2.5 text-primary-text bg-transparent font-medium hover:text-primary-background hover:bg-primary'
-                    onClick={() => navigate(-1)}>
-                    <IoMdArrowBack size={16} />
-                    Go back
-                  </button>
-                  {MediaStore.current.homepage && (
-                    <a
-                      href={MediaStore.current.homepage}
-                      target='_blank'
-                      rel='noopener noreferrer'
-                      type='button'
-                      className='text-sm rounded-xl border-2 border-primary flex items-center gap-x-2 pl-3 pr-4 py-2.5 text-primary-background hover:text-primary-background bg-primary hover:ring hover:ring-orange-50 hover:border-primary ring-offset-0 font-medium '
-                      onClick={() => {}}>
-                      <IoIosLink size={16} />
-                      View on website
-                    </a>
+                  </div>
+                  {/* Media official tagline if exist */}
+                  {MediaStore.current.tagline && (
+                    <p className='italic text-secondary-text text-base mb-4'> {MediaStore.current.tagline} </p>
                   )}
+                  {/* Media overview */}
+                  <h3 className='text-2xl font-bold'>Overview</h3>
+                  <p className='mt-2'>{MediaStore.current.overview && MediaStore.current.overview}</p>
+                  {/* button group */}
+                  <div className='mt-7 w-fit flex gap-5'>
+                    <button
+                      type='button'
+                      className='text-sm rounded-xl border-2 border-primary flex items-center gap-x-2 pl-3 pr-4 py-2.5 text-primary-text bg-transparent font-medium hover:ring hover:ring-orange-50 hover:border-primary ring-offset-0'
+                      onClick={() => navigate(-1)}>
+                      <IoMdArrowBack size={16} />
+                      Go back
+                    </button>
+                    {MediaStore.current.homepage && (
+                      <a
+                        href={MediaStore.current.homepage}
+                        target='_blank'
+                        rel='noopener noreferrer'
+                        type='button'
+                        className='text-sm rounded-xl border-2 border-primary flex items-center gap-x-2 pl-3 pr-4 py-2.5 text-primary-background hover:text-primary-background bg-primary hover:ring hover:ring-orange-50 hover:border-primary ring-offset-0 font-medium'
+                        onClick={() => {}}>
+                        <IoIosLink size={16} />
+                        View on website
+                      </a>
+                    )}
+                  </div>
                 </div>
-              </div>
-            </section>
-          </div>
+
+                {/* seasons details */}
+                <div className='mt-8'>
+                  <h3 className='text-2xl font-bold'>
+                    Has {MediaStore.current.number_of_episodes && MediaStore.current.number_of_episodes} episodes for{' '}
+                    {MediaStore.current.number_of_seasons && MediaStore.current.number_of_seasons} seasons
+                  </h3>
+                  <div className='mt-2'>
+                    {/* season list */}
+                    <div className='flex flex-row flex-nowrap py-2 gap-3 overflow-x-scroll'>
+                      {/* season card details */}
+                      {MediaStore.current.seasons &&
+                        MediaStore.current.seasons.map((season, index) => {
+                          if (!season.season_number == 0) {
+                            return (
+                              <div key={index} className='p-4 border border-gray-500 rounded w-96 flex-shrink-0'>
+                                <div className='flex justify-between'>
+                                  <span className='text-primary'>{season.name && season.name}</span>
+                                  <span className='text-secondary-text text-xs'>
+                                    {season.air_date && new Date(season.air_date).getFullYear()}
+                                  </span>
+                                </div>
+                                <p className='text-sm text-justify mt-2'>{season.overview && season.overview}</p>
+                              </div>
+                            );
+                          }
+                        })}
+                    </div>
+                  </div>
+                </div>
+              </section>
+            </div>
+          </>
         )
       ) : (
         // loading skeleton
@@ -103,7 +139,7 @@ function DetailsPage() {
               <Skeleton className='h-6 mt-6 w-full' width={250} />
               {/* Media overview */}
               {/* <h3 className='text-2xl font-bold'></h3> */}
-              <Skeleton className='mt-4 h-8' width={106} />
+              <Skeleton className='mt-4 h-7' width={116} />
               {/* <p className='mt-2'></p> */}
               <div className='mt-2'>
                 <Skeleton className='h-4' count={3} />
