fc2fd4b35680cb7e8838f136f8725662518b0be7
mifalia <mifarivonjisoa@gmail.com>
2024-01-23 13:43:53 +0300

    fix title size responsiveness

--- src/pages/discover/DiscoverPage.jsx
+++ src/pages/discover/DiscoverPage.jsx
@@ -16,24 +16,16 @@ function DiscoverPage() {
     <HeaderFooterLayout>
       <div className='container mx-auto px-4 pt-24 pb-10 text-primary-text'>
         {/* trending */}
-        <h2 className='uppercase text-center text-3xl font-bold font-sans mt-6 mb-10'>
+        <h2 className='uppercase text-center text-2xl sm:text-3xl font-bold font-sans mt-2 mb-6 md:mt-6 md:mb-10'>
           Popular this week
         </h2>
-        {MediaStore.isLoading ? (
-          <MediaListSkeleton />
-        ) : (
-          <MediaList mediaList={MediaStore.trending} />
-        )}
+        {MediaStore.isLoading ? <MediaListSkeleton /> : <MediaList mediaList={MediaStore.trending} />}
         {/* popular */}
 
-        <h2 className='uppercase text-center text-3xl font-bold font-sans mt-14 mb-10'>
+        <h2 className='uppercase text-center text-2xl sm:text-3xl font-bold font-sans mt-12 mb-8 md:mt-14 md:mb-10'>
           All time popular
         </h2>
-        {MediaStore.isLoading ? (
-          <MediaListSkeleton />
-        ) : (
-          <MediaList mediaList={MediaStore.popular} />
-        )}
+        {MediaStore.isLoading ? <MediaListSkeleton /> : <MediaList mediaList={MediaStore.popular} />}
       </div>
     </HeaderFooterLayout>
   );
