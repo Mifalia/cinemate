796c3cb1f6bcb78b5d463d9e1325b5309ae39189
mifalia <mifarivonjisoa@gmail.com>
2024-01-24 15:43:40 +0300

    fix homepage responsiveness

--- src/pages/home/HomePage.jsx
+++ src/pages/home/HomePage.jsx
@@ -5,7 +5,7 @@ import { observer } from 'mobx-react';
 import { RiSearchLine } from 'react-icons/ri';
 import { IoMdArrowForward } from 'react-icons/io';
 // assets import
-import s from './HomePage.module.css';
+import backgroundImage from 'assets/images/main-bg.png';
 // component imports
 import { MediaStore } from 'store/MediaStore';
 import MediaList from 'components/media/MediaList/MediaList';
@@ -21,38 +21,41 @@ function HomePage() {
 
   return (
     <HeaderFooterLayout>
-      <div className={`${s['main-wrapper']}`}>
-        <section
-          className={`${s['hero']} container mx-auto px-5 h-[calc(100vh-48px)] py-24 flex flex-col justify-center`}>
-          <div className='w-1/2'>
-            <h1 className='section-title text-3xl font-bold font-sans mb-2'>
+      <div className='min-h-[calc(100vh-72px)]'>
+        <div
+          className='h-[calc(100vh-48px)] flex flex-col justify-center mx-auto container px-4'
+          style={{
+            background: `linear-gradient(to right, #202020, rgba(0, 0, 0, 0.55)),url(${backgroundImage}) no-repeat top center / cover`,
+          }}>
+          <div className='w-full md:w-5/6 lg:w-3/4 xl:w-1/2'>
+            <h1 className='section-title text-2xl sm:text-3xl font-bold font-sans mb-2'>
               Welcome to <span className='text-primary'>Cinemate</span>
             </h1>
-            <p className='section-description text-6xl font-bold font-sans mb-6 capitalize'>
+            <p className='section-description text-4xl sm:text-6xl font-bold font-sans mb-6 capitalize'>
               explore the world of TV Shows & more
             </p>
-            <p className={`${s['body-link-wrapper']} mb-12 w-fit font-sans`}>
-              <Link className={`underline text-base leading-loose`} to='/about'>
+            <p className='w-fit py-0 font-sans transition-transform transform hover:translate-x-4'>
+              <Link className='underline text-base leading-loose' to='/about'>
                 Learn more about us
                 <IoMdArrowForward size={16} className='ml-2 inline-block' />
               </Link>
             </p>
             <button
-              className='rounded-xl border-2 border-primary flex items-center gap-x-3 pl-6 pr-8 py-4 text-primary-text bg-transparent font-medium hover:text-primary-background hover:bg-primary'
+              className='w-full justify-center sm:w-fit sm:justify-start mt-24 sm:mt-12 rounded-xl border-2 border-primary flex items-center gap-x-3 pl-6 pr-8 py-4 text-primary-text bg-transparent font-medium hover:text-primary-background hover:bg-primary'
               onClick={() => navigate('/discover')}>
               <RiSearchLine size={18} />
               Start exploring
             </button>
           </div>
-        </section>
-      </div>
-
-      <section className='w-full bg-primary-background py-14'>
-        <div className='container mx-auto'>
-          <h2 className='capitalize  text-center text-3xl font-bold font-sans mb-9'>You may like ...</h2>
-          {MediaStore.isLoading ? <MediaListSkeleton /> : <MediaList mediaList={MediaStore.trending.slice(0, 5)} />}
         </div>
-      </section>
+
+        <div className='w-full bg-primary-background py-14'>
+          <div className='container mx-auto px-4'>
+            <h2 className='capitalize  text-center text-2xl sm:text-3xl font-bold font-sans mb-9'>You may like ...</h2>
+            {MediaStore.isLoading ? <MediaListSkeleton /> : <MediaList mediaList={MediaStore.trending.slice(0, 5)} />}
+          </div>
+        </div>
+      </div>
     </HeaderFooterLayout>
   );
 }
